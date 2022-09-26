d74e44fd2de079b8096d287b2c9b0ed8f6a4a058
JakubJarosz <jakubtm165@gmail.com>
2022-09-26 13:05:20 +0200

    Updates

--- static/js/246.c7faf7b1.chunk.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgoit_react_hw_05_movies=self.webpackChunkgoit_react_hw_05_movies||[]).push([[246],{1246:function(e,i,n){n.r(i),n.d(i,{default:function(){return c}});n(2791);var r=n(1087),t=n(184);function c(e){return(0,t.jsxs)("div",{children:[(0,t.jsx)("h1",{children:"Tranding today"}),(0,t.jsx)("ul",{children:e.PopularMovies&&e.PopularMovies.map((function(e){return(0,t.jsx)("li",{children:(0,t.jsx)(r.rU,{to:"/movie/".concat(e.id),children:e.title||e.name})},e.id)}))})]})}}}]);
-//# sourceMappingURL=246.c7faf7b1.chunk.js.map
