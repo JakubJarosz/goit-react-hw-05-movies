1b86c649f104317d97169dae81975e13529eddc1
JakubJarosz <jakubtm165@gmail.com>
2022-09-26 11:45:25 +0200

    ad

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import './index.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <HashRouter>
+    <HashRouter basename="/goit-react-hw-05-movies">
       <App />
     </HashRouter>
   </React.StrictMode>
